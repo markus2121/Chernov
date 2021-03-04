76787c04d8fa7ce3ef001bd17a48eeede5653740
markus2121 <72490543+markus2121@users.noreply.github.com>
2021-03-04 18:11:52 +0300

    Add files via upload

---
+++ Mobile-App-Development-main/Student/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Student
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+
+            count++;
+            res.Text = "Студентов: " + count.ToString();
+        }
+
+        private void Del_Clicked(object sender, EventArgs e)
+        {
+            count--;
+            res.Text = "Студентов: " + count.ToString();
+        }
+    }
+}
---
+++ Mobile-App-Development-main/Traffic_light/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Traffic_light
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void red_Clicked(object sender, EventArgs e)
+        {
+            Main.BackgroundColor = Color.Red;
+        }
+
+        private void yellow_Clicked(object sender, EventArgs e)
+        {
+            Main.BackgroundColor = Color.Yellow;
+        }
+
+        private void green_Clicked(object sender, EventArgs e)
+        {
+            Main.BackgroundColor = Color.Green;
+        }
+    }
+}
